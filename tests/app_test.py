be5710f86291c4e70fc610eb46cc8dbc085778c0
Zakir Gowani <zakirgowani@gmail.com>
2020-10-16 00:34:40 -0500

    debugging

--- tests/app_test.py
+++ tests/app_test.py
@@ -68,6 +68,11 @@ def app(mocker):
         "manifestservice.manifests._get_file_contents", return_value=""
     )
 
+    mocks["_add_GUID_to_bucket"] = mocker.patch(
+        "manifestservice.manifests._add_GUID_to_bucket",
+        return_value=("a-guid-value", True),
+    )
+
     app = create_app()
     return app
 
